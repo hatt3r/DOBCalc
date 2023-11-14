780c9e62ad2e6db178ba03d44aff8fe4a25e9831
Serene <serene719@gmail.com>
2023-11-14 13:33:11 +0530

    changing UI and adding date picker

--- app/src/main/java/com/cessabit/dobcalc/MainActivity.kt
+++ app/src/main/java/com/cessabit/dobcalc/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.cessabit.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
+import java.time.Year
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+        }
+
+    }
+    fun clickDatePicker(){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{ view,year,month,dayOfMonth ->
+                Toast.makeText(this,"Date Picker works",Toast.LENGTH_LONG).show()
+            },
+            year,month,day
+        ).show()
+
     }
 }
